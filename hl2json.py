1e0558fad62830c71d12916ff1fc76a4f51a4bb1
Blesmol <Blesmol@users.noreply.github.com>
2020-05-04 17:32:27 +0200

    Support text and html as output format

--- hl2json.py
+++ hl2json.py
@@ -11,11 +11,14 @@ def main():
     ap = argparse.ArgumentParser(description='Extract Pathfinder characters from HeroLab files and store them locally in various formats')
     ap.add_argument('file', help='HeroLab *.por file', nargs='+')
     ap.add_argument('-j', '--json', help='Extract JSON (Default if nothing else is selected)', action='store_true')
-    ap.add_argument('-x', '--xml', help='Extract XML', action='store_true')
+    ap.add_argument('-x', '--xml', help='Extract XML Statblock', action='store_true')
+    ap.add_argument(      '--html', help='Extract HTML Statblock', action='store_true')
+    ap.add_argument('-t', '--text', help='Extract text statblock', action='store_true')
+    ap.add_argument('-d', '--debug', help='Debug output', action='store_true')
     args = ap.parse_args()
 
-    # set json output as default
-    if not args.xml:
+    # set json output as default if no other output flags were set
+    if not args.xml and not args.html and not args.text:
         args.json = True
 
     for hl_filename in args.file:
@@ -25,29 +28,48 @@ def main():
         with zipfile.ZipFile(hl_filename, 'r') as zip:
             infolist = zip.infolist()
             for entry in infolist:
-                if entry.filename.startswith('statblocks_xml'):
+                if args.debug:
+                    print(str(entry))
+
+                if not entry.filename.startswith('statblocks_'):
+                    continue
+
+                char_name = get_char_name(entry.filename)
+                base_output_filename = '{} - {}'.format(hl_basename, char_name)
+
+                if args.html and entry.filename.startswith('statblocks_html'):
+                    extract_into_file(zip, entry.filename, base_output_filename, 'html')
+
+                if args.text and entry.filename.startswith('statblocks_text'):
+                    extract_into_file(zip, entry.filename, base_output_filename, 'txt')
+
+                if args.xml and entry.filename.startswith('statblocks_xml'):
+                    extract_into_file(zip, entry.filename, base_output_filename, 'xml')
+
+                if args.json and entry.filename.startswith('statblocks_xml'):
                     data = zip.read(entry.filename)
-                    base_source_filename = os.path.splitext(os.path.basename(entry.filename))[0]
 
-                    prefix_endpos = base_source_filename.find('_')
-                    assert prefix_endpos != -1
-                    char_name = base_source_filename[prefix_endpos+1:]
+                    json_filename = '{}.json'.format(base_output_filename)
+                    with open(json_filename, 'w') as json_file:
+                        data_dict = xmltodict.parse(data, attr_prefix='_', postprocessor=(
+                            lambda _, key, value: (key if key else "", value if value else ""))
+                        )
+                        json_file.write(json.dumps(data_dict, indent='\t'))
 
-                    base_output_filename = '{} - {}'.format(hl_basename, char_name)
+def extract_into_file(zip, zip_filename, output_basename, output_extension):
+    output_filename = '{}.{}'.format(output_basename, output_extension)
+    with open(output_filename, 'wb') as file:
+        data = zip.read(zip_filename)
+        file.write(data)
 
-                    if args.xml:
-                        xml_filename = '{}.xml'.format(base_output_filename)
-                        with open(xml_filename, 'wb') as xml_file:
-                            xml_file.write(data)
+def get_char_name(full_filename):
+    filename = os.path.basename(full_filename)
+    base_filename = os.path.splitext(filename)[0]
 
-                    if args.json:
-                        json_filename = '{}.json'.format(base_output_filename)
-                        with open(json_filename, 'w') as json_file:
-                            data_dict = xmltodict.parse(data, attr_prefix='_', postprocessor=(
-                                lambda _, key, value: (key if key else "", value if value else ""))
-                            )
-                            json_file.write(json.dumps(data_dict, indent='\t'))
-
+    # Char filenames in HeroLab have pattern '<index>_<char>.<ext>'
+    prefix_endpos = base_filename.find('_')
+    assert prefix_endpos != -1
+    return base_filename[prefix_endpos+1:]
 
 if __name__=="__main__":
     main()
