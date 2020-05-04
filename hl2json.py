b69c43591348f0cc4f2edcfbcb63bfad0514cc64
Blesmol <Blesmol@users.noreply.github.com>
2020-05-04 23:31:25 +0200

    add get_number_of_chars()

--- hl2json.py
+++ hl2json.py
@@ -10,10 +10,10 @@ import argparse
 def main():
     ap = argparse.ArgumentParser(description='Extract Pathfinder characters from HeroLab files and store them locally in various formats')
     ap.add_argument('file', help='HeroLab *.por file', nargs='+')
-    ap.add_argument('-j', '--json',  help='Extract JSON (Default if nothing else is selected)', action='store_true')
-    ap.add_argument('-x', '--xml',   help='Extract XML Statblock', action='store_true')
-    ap.add_argument(      '--html',  help='Extract HTML Statblock', action='store_true')
-    ap.add_argument('-t', '--text',  help='Extract text statblock', action='store_true')
+    ap.add_argument('-j', '--json', help='Extract JSON (Default if nothing else is selected)', action='store_true')
+    ap.add_argument('-x', '--xml', help='Extract XML Statblock', action='store_true')
+    ap.add_argument(      '--html', help='Extract HTML Statblock', action='store_true')
+    ap.add_argument('-t', '--text', help='Extract text statblock', action='store_true')
     ap.add_argument('-d', '--debug', help='Debug output', action='store_true')
     args = ap.parse_args()
 
@@ -26,6 +26,8 @@ def main():
 
         assert zipfile.is_zipfile(hl_filename)
         with zipfile.ZipFile(hl_filename, 'r') as zip:
+            num_chars = get_number_of_chars(zip) # not yet used
+
             infolist = zip.infolist()
             for entry in infolist:
                 if args.debug:
@@ -35,7 +37,10 @@ def main():
                     continue
 
                 char_name = get_char_name(entry.filename)
-                base_output_filename = '{} - {}'.format(hl_basename, char_name)
+                if hl_basename != char_name:
+                    base_output_filename = '{} - {}'.format(hl_basename, char_name)
+                else:
+                    base_output_filename = hl_basename
 
                 if args.html and entry.filename.startswith('statblocks_html'):
                     extract_into_file(zip, entry.filename, base_output_filename, 'html')
@@ -67,9 +72,18 @@ def get_char_name(full_filename):
     base_filename = os.path.splitext(filename)[0]
 
     # Char filenames in HeroLab have pattern '<index>_<char>.<ext>'
+    # If the char name contains spaces, they are replace by underscores
+
     prefix_endpos = base_filename.find('_')
     assert prefix_endpos != -1
-    return base_filename[prefix_endpos+1:]
+    return base_filename[prefix_endpos+1:].replace('_', ' ')
+
+def get_number_of_chars(zip):
+    count = 0
+    for entry in zip.infolist():
+        if entry.filename.startswith('statblocks_xml'):
+            count+=1
+    return count
 
 if __name__=="__main__":
     main()
