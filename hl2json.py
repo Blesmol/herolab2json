708dbba8eae4521992f4fe1fcbb7cc98acea110e
Blesmol <Blesmol@users.noreply.github.com>
2020-05-05 22:48:25 +0200

    add proper minion json extraction

--- hl2json.py
+++ hl2json.py
@@ -6,6 +6,7 @@ import sys
 import zipfile
 import os
 import argparse
+import copy
 
 def main():
     ap = argparse.ArgumentParser(description='Extract Pathfinder characters from HeroLab files and store them locally in various formats')
@@ -53,13 +54,31 @@ def main():
 
                 if args.json and entry.filename.startswith('statblocks_xml'):
                     data = zip.read(entry.filename)
+                    char_dict = xmltodict.parse(data, attr_prefix='_', postprocessor=(
+                        lambda _, key, value: (key if key else "", value if value else ""))
+                    )
 
                     json_filename = '{}.json'.format(base_output_filename)
                     with open(json_filename, 'w') as json_file:
-                        data_dict = xmltodict.parse(data, attr_prefix='_', postprocessor=(
-                            lambda _, key, value: (key if key else "", value if value else ""))
-                        )
-                        json_file.write(json.dumps(data_dict, indent='\t'))
+                        json_file.write(json.dumps(char_dict, indent='\t'))
+
+                    # check for minions; if one exists, extract into separate file
+                    # TODO check if multiple minions can exist for one char, and if so, how they are stored
+                    minion_dict = get_minions(char_dict)
+                    if minion_dict:
+                        minion_name = minion_dict['character']['_name']
+                        minion_json_filename = '{} - {}.json'.format(base_output_filename, minion_name)
+
+                        # The dict is missing some expected entries for completeness. Lets be lazy, copy
+                        # the char dict and only replace the character with the minion
+                        minion_dict_complete = copy.deepcopy(char_dict)
+                        minion_dict_complete['document']['public']['character'] = minion_dict['character']
+
+                        if args.debug:
+                            print('Minion {} exists for char {}'.format(minion_name, char_name))
+                        with open(minion_json_filename, 'w') as json_file:
+                            json_file.write(json.dumps(minion_dict_complete, indent='\t'))
+
 
 def extract_into_file(zip, zip_filename, output_basename, output_extension):
     output_filename = '{}.{}'.format(output_basename, output_extension)
@@ -72,11 +91,22 @@ def get_char_name(full_filename):
     base_filename = os.path.splitext(filename)[0]
 
     # Char filenames in HeroLab have pattern '<index>_<char>.<ext>'
-    # If the char name contains spaces, they are replace by underscores
+    # If the char name contains spaces, they are replaced by underscores
 
     prefix_endpos = base_filename.find('_')
     assert prefix_endpos != -1
     return base_filename[prefix_endpos+1:].replace('_', ' ')
+
+def get_minions(char_dict):
+    keys = ['document', 'public', 'character', 'minions']
+    curr = char_dict
+    for key in keys:
+        curr = curr.get(key)
+        if not curr or not issubclass(type(curr), dict):
+            return None
+    # if no minions exist, then the last entry will only be an empty
+    # string and thus no subclass of dict, i.e. None is returned
+    return curr
 
 def get_number_of_chars(zip):
     count = 0
